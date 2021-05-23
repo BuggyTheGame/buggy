8fcde19d0c84913debf46033755fae0814eb82b0
Yanis <59359557+yanis-man@users.noreply.github.com>
2021-05-23 21:57:27 +0200

    manual update
    few changes on commands manual

--- js/terminal/commands/man.js
+++ js/terminal/commands/man.js
@@ -1,7 +1,7 @@
 export const MAN_DATA = 
     {
-        "echo" : "Permet d'afficher le texte passé en paramètre",
+        "echo" : "Permet d'afficher le texte passé en paramètre <br/> echo {message} <br/>",
         "reboot" : "Redémarre les machines",
-        "hire_devs" : "Sonde les pires sites de recrutements à la recherche de devs",
-        "apt" : "Installe les dernières mise à jours les plus buggées"
+        "hire" : "Sonde les pires sites de recrutements à la recherche de devs <br/> hire dev/hacker {quantité}",
+        "apt" : "Installe les dernières mise à jours les plus buggées <br/> apt list/install {id}"
     }
